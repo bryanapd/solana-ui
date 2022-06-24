a20d8b82f5ca2ec9cef806b657c85dfc44370879
Bryan Diolata <bryan.appdeture@gmail.com>
2022-06-24 19:12:34 +0800

    Updated Hero and Feature Section

--- components/Feature.js
+++ components/Feature.js
@@ -1,28 +1,31 @@
-import { Box, Text, Stack, AspectRatio, Heading, position } from '@chakra-ui/react'
+import { Box, Text, Stack, AspectRatio, Heading, position, Button } from '@chakra-ui/react'
 import { ThemedButton } from './Layout'
 
 export const GreetingCard = ({ title = 'Say hello' }) => (
-  <Box align="center">
-    <Box p={5} h="280px" w={{base: 'auto', md: '500px'}} top="20" right="0" bg="#19F095" color="black" borderRadius={10}
-      pos={{ md: '', lg: 'absolute' }} zIndex={999} 
-    >
-      <Heading size="4xl" pb={5}>{title}</Heading>
-      <Text fontSize="lg" pb={10}>Learn more about Solana and <br /> see what makes it great.</Text>
-      <ThemedButton title="WATCH VIDEO"/>
+    <Box 
+      p={5} 
+      h={{ base: 'auto', md: "280px" }}
+      w={{ base: 'auto', md: '500px' }} 
+      textAlign="center" 
+      top="20" right="0" 
+      bg="#19F095" color="black" 
+      pos={{ base: 'relative', md: 'absolute' }} 
+      borderRadius="lg"
+      zIndex={999}>
+      <Heading size="4xl" mb={5}>{title}</Heading>
+      <Text fontSize="lg" mb={10}>Learn more about Solana and <br /> see what makes it great.</Text>
+      <Button size="md" bg="black" color="white" rounded="full">WATCH VIDEO</Button>
     </Box>
-    {/* <Box pos="absolute" h="80px" w="500px" top="0" right="0" bg="#19F095" /> */}
-  </Box>
-  
 )
 export const Feature = () => (
   <Box mt={10} pos="relative"> 
     <GreetingCard />
     <AspectRatio>
       <iframe 
-      showinfo="0" allow="autoplay" allow 
-      src=
-      "https://player.vimeo.com/external/589416920.sd.mp4?s=2cadb33b13c6414622f3ab975b959029b54018e1&profile_id=165&autoplay=1" 
-      />
+        showinfo="0" 
+        allow="autoplay" 
+        src="https://player.vimeo.com/external/589416920.sd.mp4?s=2cadb33b13c6414622f3ab975b959029b54018e1&profile_id=165&autoplay=1" 
+        />
     </AspectRatio>
   </Box>
 )
--- components/Hero.js
+++ components/Hero.js
@@ -3,26 +3,30 @@ import {
   Box, Container
 } from '@chakra-ui/react';
 export const Hero = () => (
-  <Flex flexDir={{ base: 'column', md: 'row' }} minH="80vh" justify={{ base: 'center', md: 'normal'}} pos="relative" pb={20}>
-    <Box w="100%">
-      <Image h="80vh" w="100%" objectFit="cover" src="https://solana.com/src/img/index/hero-wide.jpg" />
-    </Box>
+  <Box minH="80vh" bgSize="cover" bgPos="center" bgImg="https://solana.com/src/img/index/hero-wide.jpg" >
+    <Container maxW="container.95vw">
+      <Flex flexDir={{ base: 'column', md: 'row' }} minH="80vh" justify={{ base: 'center', md: 'normal'}} pos="relative" pb={20}>
+        {/* <Box w="100%">
+          <Image h="80vh" w="100%" objectFit="cover" src="https://solana.com/src/img/index/hero-wide.jpg" />
+        </Box> */}
 
-    <Stack 
-      px={{ base: 'auto', md: 20 }} spacing={6} w="full" pos="absolute" top="20" color="white" 
-      textAlign={{ base: 'center', md: 'left' }}
-      >
-      <Heading fontSize={{ base: '4xl', md: '85px' }}>Powerful for <br /> developers. <br />Fast for everyone.</Heading>
-      <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
-        <Button p={6} rounded="full" bg="primary.tealgreen" color="black">START BUILDING</Button>
-        <Button p={6} rounded="full" variant="outline" color="white" _hover={{ color: 'black', bg: 'primary.tealgreen' }}>
-          READ DOCUMENTATION
-        </Button>
-      </Stack>
-      <Text fontSize={{ base: '', md: 'xl' }}>
-        Solana is a decentralized blockchain built to enable 
-        scalable, user-friendly apps for the world.
-      </Text>
-    </Stack>
-  </Flex>
+        <Stack 
+          px={{ base: 'auto', md: 20 }} spacing={6} w="full" pos="absolute" top="20" color="white" 
+          textAlign={{ base: 'center', md: 'left' }}
+          >
+          <Heading fontSize={{ base: '4xl', md: '85px' }}>Powerful for <br /> developers. <br />Fast for everyone.</Heading>
+          <Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+            <Button p={6} rounded="full" bg="primary.tealgreen" color="black">START BUILDING</Button>
+            <Button p={6} rounded="full" variant="outline" color="white" _hover={{ color: 'black', bg: 'primary.tealgreen' }}>
+              READ DOCUMENTATION
+            </Button>
+          </Stack>
+          <Text fontSize={{ base: '', md: 'xl' }}>
+            Solana is a decentralized blockchain built to enable 
+            scalable, user-friendly apps for the world.
+          </Text>
+        </Stack>
+      </Flex>
+    </Container>
+  </Box>
 )
